7d75068437d6c8bee5de2219deecd10e1234da08
NerakCM <53309112+NerakCM@users.noreply.github.com>
2023-03-03 01:41:58 -0600

    Agregué algunos métodos

--- Criptografia.java
+++ Criptografia.java
@@ -1,36 +1,144 @@
+import java.util.*;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Enumeration;
+import java.text.DecimalFormat;
 
 public class Criptografia {
 
-    private static Hashtable<String, Integer> conteoDeCaracteres(String texto){
+
+    /**
+     * Método que le quita espacios, dígitos y caracteres especiales a un texto.
+     * @param texto es el texto a limpiar.
+     * @return el texto limpio convertido a mayúsculas.
+     */
+    public static String limpiaTexto(String texto){
+        texto = texto.toUpperCase();
+        String textoSinEspacio = texto.replace(" ", "");
+        String textoLimpio = textoSinEspacio.replaceAll("[^A-Z]", "");
+        return textoLimpio;
+    }
+
+
+    /**
+     * Método que divide en subcadenas un texto.
+     * @param texto es el texto a dividir.
+     * @param numLetras es el número de letras en el que será divido el texto.
+     * @return el texto dividido de acuerdo al número de letras.
+     */
+    public static String separaTexto(String texto, int numLetras){
+        int contador = numLetras;
+        String textoAux = "";
+        for (int i = 0; i < texto.length(); i++){
+            String letra = "" + texto.charAt(i);
+            textoAux += letra;
+            if(contador == (i+1)){
+                textoAux += " ";
+                contador += numLetras;
+            } 
+        }
+        return textoAux;
+    }
+
+
+    /**
+     * Método que nos da el porcentaje de apariciones de una letra en el texto.
+     * @param conteoCaracteres es la tabla hash que contiene las aparciones de la letra en el texto.
+     * @param texto es el texto a dividir.
+     * @return una tabla has con la letra y su porcentaje de apariciones.
+     */
+    public static Hashtable<String, String> porcentajeLetra(Hashtable<String, Integer> conteoCaracteres, String texto){
+        Hashtable<String, String> porcentajes = new Hashtable<>();  
+        int longTexto = texto.length();
+        DecimalFormat formato = new DecimalFormat("#.000");
+
+        //Para iterar en la tabla hash
+        Enumeration<String> enu = conteoCaracteres.keys();
+
+        while (enu.hasMoreElements()) {
+ 
+            String key = enu.nextElement();
+            int repeticiones = conteoCaracteres.get(key);
+            double porcentaje = (double) (repeticiones * 100) / longTexto;
+            String porcRedondeado = formato.format(porcentaje) + "%";
+            porcentajes.put(key, porcRedondeado);
+        }
+
+        return porcentajes;  
+    }
+
+
+
+    public static Hashtable<String, Integer> conteoDeCaracteres(String texto){
         Hashtable<String, Integer> frecuencias = new Hashtable<>();  
-        for(int i=0; i<texto.length(); i++){
-            String letra =""+texto.charAt(i);
-            if(texto.charAt(i)+" " ==" "){
-                frecuencias.put(" ", 1);
-            }else{
-                if(frecuencias.containsKey(letra)==true){
-                    frecuencias.put(letra, frecuencias.get(letra)+1);
-                }else{
-                    frecuencias.put(letra, 1);
-                }
-                
+        for(int i = 0; i < texto.length(); i++){
+            String letra = "" + texto.charAt(i);
+            if(frecuencias.containsKey(letra)){
+                frecuencias.put(letra, frecuencias.get(letra) + 1);
+            } else {
+                frecuencias.put(letra, 1);
             }
         }
-        return frecuencias;
-        
+        return frecuencias;  
     }
 
-    private static void porLetra(Hashtable<String, Integer> frecuencias){
-        
-    }
+
+    /**
+     * MÉTODO QUE TENÍA ANTES MI POMPA BONITA. 
+     * ES IGUAL AL DE ARRIBA PERO LE BORRÉ LINEAS DE CÓDIGO DONDE SE VERIFICAN 
+     * ESPACIOS PORQUE YA LO HACE EL MÉTODO QUE LIMPIA EL TEXTO.
+     */
+
+    //private static Hashtable<String, Integer> conteoDeCaracteres(String texto){
+    //    Hashtable<String, Integer> frecuencias = new Hashtable<>();  
+    //    for(int i=0; i<texto.length(); i++){
+    //        String letra =""+texto.charAt(i);
+    //        if(texto.charAt(i)+" " ==" "){
+    //            frecuencias.put(" ", 1);
+    //        }else{
+    //            if(frecuencias.containsKey(letra)==true){
+    //                frecuencias.put(letra, frecuencias.get(letra)+1);
+    //            }else{
+    //                frecuencias.put(letra, 1);
+    //            } 
+    //        }
+    //    }
+    //    return frecuencias;  
+    //}
+
+
 
     public static void main(String[] args){
+
+        System.out.println("\nTEXTO:");
+        String texto = "hola mucho gusto uwu";
+        System.out.println(texto);
+        System.out.println("______________________");
+
+
+        //  Prueba del método que cuenta las frecuencias de una letra en el texto.
+        System.out.println("\n PRUEBA DE FRECUENCIAS EN EL TEXTO");
         Hashtable<String, Integer> frecuencias = new Hashtable<>(); 
-        frecuencias = conteoDeCaracteres("hola mucho gusto uwu");
+        frecuencias = conteoDeCaracteres(limpiaTexto(texto));
         System.out.println(frecuencias);
-        
-    }
-    
+        System.out.println("______________________");
+
+        //  Prueba del método que limpia texto
+        System.out.println("\n PRUEBA DE LIMPIAR TEXTO");
+        System.out.println(limpiaTexto("Hola, # muCh0o @ ! Gusto $u/w.u"));
+        System.out.println("______________________");
+
+        //  Prueba del método que divide el texto en n letras.
+        System.out.println("\n PRUEBA DE DIVIDIR TEXTO EN 2");
+        System.out.println(separaTexto(limpiaTexto(texto), 2));
+        System.out.println("______________________");
+
+        //  Prueba del método que da el porcentaje de cuánto aparece una letra en el texto.
+        System.out.println("\n PRUEBA DE PORCENTAJE DE APARICIÓN DE LETRA");
+        Hashtable<String, String> porcentajes = new Hashtable<>(); 
+        porcentajes = porcentajeLetra(frecuencias, limpiaTexto(texto));
+        System.out.println(porcentajes);
+        System.out.println("______________________");
+
+    } 
 }
