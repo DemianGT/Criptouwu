969dcd184b69559aa0192293295529cb735c1efb
Demian Oswaldo Garcia Toxqui <demiangt17@ciencias.unam.mx>
2023-03-14 14:12:20 -0600

    Agregue los metodos de cifrado con clave y descifrado con clave, podria decirse que ya estan los monoalfabeticos uwu

--- AlgoritmosCifrado.java
+++ AlgoritmosCifrado.java
@@ -79,6 +79,58 @@ public class AlgoritmosCifrado extends AlgoritmosBasicos{
         return resultado;
     }
 
+    public static String abecedarioConClave(String clave){
+
+        String claveSinRepetir = "";
+
+        for (int i = 0; i < clave.length(); i++) {
+            char c = clave.charAt(i);
+            if(claveSinRepetir.indexOf(c) == -1){
+                claveSinRepetir+=c;
+            }
+        }
+
+        String abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder sb = new StringBuilder(abecedario);
+        for (int i = 0; i < claveSinRepetir.length(); i++) {
+            char c = claveSinRepetir.charAt(i);
+            int indice = sb.indexOf(String.valueOf(c));
+            if (indice != -1) {
+                sb.deleteCharAt(indice);
+            }
+        }
+        String abecedarioConClave = claveSinRepetir + sb.toString();
+        return abecedarioConClave;
+    }
+
+    public static String cifradoClave(String texto, String clave) {
+        String abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String abecedarioConClave = "";
+        abecedarioConClave = abecedarioConClave(clave);
+        System.out.println("Abecedario con clave: " + abecedarioConClave);
+        String resultado = "";
+        for (int i = 0; i < texto.length(); i++) {
+            int indiceOriginal = abecedario.indexOf(texto.charAt(i));
+            resultado+=abecedarioConClave.charAt(indiceOriginal);
+        }
+        return resultado;
+    }
+
+    public static String descifradoClave(String texto, String clave) {
+        String abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String abecedarioConClave = "";
+        abecedarioConClave = abecedarioConClave(clave);
+        System.out.println("Abecedario con clave: " + abecedarioConClave);
+        String resultado = "";
+        for (int i = 0; i < texto.length(); i++) {
+            int indiceOriginal = abecedarioConClave.indexOf(texto.charAt(i));
+            resultado+=abecedario.charAt(indiceOriginal);
+        }
+        return resultado;
+    }
+
+
+
 
     public static void main(String[] args){
 
@@ -106,15 +158,18 @@ public class AlgoritmosCifrado extends AlgoritmosBasicos{
         String anterior = "";
         String nuevo = "";
         int rotacion = 0;
+        String clave = "";
         int m = 0;
         int a = 0;
 
         while (opcion != 20) {
             System.out.println("\u001B[1m---- MENÚ ALGORITMOS DE CIFRADO ----\u001B[0m");
             System.out.println("1. Cifrado Cesar");
-            System.out.println("2. Descifrado Cesar");
+            System.out.println("2. Descifrado Cesar\n");
             System.out.println("3. Cifrado Afin");
             System.out.println("4. Descifrado Afin\n");
+            System.out.println("5. Cifrado clave");
+            System.out.println("6. Descifrado clave\n");
             
             System.out.print("\nElige una opción: ");
             opcion = sc.nextInt();
@@ -164,6 +219,22 @@ public class AlgoritmosCifrado extends AlgoritmosBasicos{
                     sc.nextLine();
                     
                     System.out.println(descifradoAfin(texto, m, a));
+                    break;
+                case 5:
+                    System.out.println("\n\u001B[38;5;82m\u001B[1m---- CIFRAR MEDIANTE CLAVE ----\u001B[0m\n");
+                    System.out.print("Ingrese el texto a cifrar:\n");
+                    texto = sc.nextLine();
+                    System.out.print("Ingrese la clave con la que se cifrara:\n");
+                    clave = sc.nextLine();
+                    System.out.println(cifradoClave(texto, clave));
+                    break;
+                case 6:
+                    System.out.println("\n\u001B[38;5;82m\u001B[1m---- DESCIFRAR MEDIANTE CLAVE ----\u001B[0m\n");
+                    System.out.print("Ingrese el texto a descifrar:\n");
+                    texto = sc.nextLine();
+                    System.out.print("Ingrese la clave con la que se descifrara:\n");
+                    clave = sc.nextLine();
+                    System.out.println(descifradoClave(texto, clave));
                     break;
                 case 10:
                     System.out.println("Adios uwu");
