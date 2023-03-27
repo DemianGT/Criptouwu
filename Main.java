93a2af57ea6ea81502ff8d5ca3d496239247e678
NerakCM <53309112+NerakCM@users.noreply.github.com>
2023-03-26 22:51:45 -0600

    Delete Main.java

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        AlgoritmosBasicos ab = new AlgoritmosBasicos();
-        AlgoritmosCifrado ac = new AlgoritmosCifrado();
-    }
-} 
